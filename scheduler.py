d42a4fd16ed9559863af864adfa1ed5ae44134b3
nwinston <noahwinston@Noahs-MacBook-Pro-2.local>
2019-01-10 21:42:41 -0500

    cleanup

--- scheduler.py
+++ scheduler.py
@@ -18,7 +18,7 @@ parser.add_argument('-output', type=str, nargs='?',
     default='schedule.csv', help='schedule output file')
 args = parser.parse_args()
 
-# Weights to be used when calculating the cost matrix
+# Costs
 PRECEDENCE_WEIGHT = -5
 FIRST_CHOICE      = -3
 SECOND_CHOICE     = -2
@@ -26,112 +26,113 @@ THIRD_CHOICE      = -1
 NO_PREF           = 0
 LAST_CHOICE       = 20
 
+
 def load_to_list(file):
     with open(file) as f:
         return [line.strip() for line in f.readlines()]
 
 
-class Scheduler:
-    def __init__(self, periods, workers, jobs, requests):
-        '''
-        Args:
-            - number of periods to schedule
-            - list of workers in order of precedence
-            - list of jobs
-            - list of worker requests
-        '''
-        self.periods = periods
-        self.workers = workers
-        self.jobs = jobs
-        self.requests = requests
-        self.schedule = defaultdict(list)
+def sort_requests(requests, workers):
+    '''
+    Sorts self.requests in order of precedence
+    '''
+    return sorted(requests,
+                  reverse=True,
+                  key=lambda req: workers.index(req.Name))
 
-    def _sort_requests(self):
-        '''
-        Sorts self.requests in order of precedence
-        '''
-        self.requests = sorted(requests,
-                                reverse=True,
-                                key=lambda req: self.workers.index(req.Name))
 
-    def _job_counts(self):
-        '''
-        Assigns number of jobs each worker has to work
-        '''
-        total_jobs = self.periods * len(self.jobs)
+def job_counts(periods, requests, jobs):
+    '''
+    Assigns number of jobs each worker has to work
 
-        pool = defaultdict(int)
-        for i in range(total_jobs):
-            ndx = i % len(self.requests)
-            pool[self.requests[ndx].Name] += 1
-        return pool
+    Args:
+        - number of periods
+        - list of ordered worker requests
+        - list of job names
+    '''
+    total_jobs = periods * len(jobs)
 
-    def calc_costs(self, request, initial_cost):
-        '''
-        Args:
-            - Worker's job requests
-            - base cost
-        '''
-        costs = []
-        for job in self.jobs:
-            cost = initial_cost
-            if job == request.Choice1:
-                cost += FIRST_CHOICE
-            elif job == request.Choice2:
-                cost += SECOND_CHOICE
-            elif job == request.Choice3:
-                cost += THIRD_CHOICE
-            elif job == request.ChoiceLast:
-                cost += LAST_CHOICE
-            else:
-                cost += NO_PREF
+    pool = defaultdict(int)
+    for i in range(total_jobs):
+        ndx = i % len(requests)
+        pool[requests[ndx].Name] += 1
+    return pool
 
-            costs.append(cost)
-        return np.array(costs)
 
-    def assign_schedule(self):
-        self.schedule.clear()
-        self._sort_requests()
-        pool = self._job_counts()
+def calc_costs(request, initial_cost, jobs):
+    '''
+    Args:
+        - Worker's job requests
+        - base cost
+        - list of job names
+    '''
+    costs = []
+    for job in jobs:
+        cost = initial_cost
+        if job == request.Choice1:
+            cost += FIRST_CHOICE
+        elif job == request.Choice2:
+            cost += SECOND_CHOICE
+        elif job == request.Choice3:
+            cost += THIRD_CHOICE
+        elif job == request.ChoiceLast:
+            cost += LAST_CHOICE
+        else:
+            cost += NO_PREF
 
-        min_cost = PRECEDENCE_WEIGHT * len(self.requests)
+        costs.append(cost)
+    return np.array(costs)
 
-        for period in range(self.periods):
-            cost_matrix = []
-            periods_left = self.periods - period
 
-            for ndx, request in enumerate(self.requests):
-                initial_cost = PRECEDENCE_WEIGHT * ndx
-                shifts_left = pool[request.Name]
+def assign_schedule(periods, requests, jobs):
+    '''
+    Args:
+        - number of periods
+        - list of requests
+        - list of job names
+    '''
+    counts = job_counts(periods, requests, jobs)
+    min_cost = PRECEDENCE_WEIGHT * len(requests)
 
-                # If a worker has as many shifts left as periods,
-                # they get highest precedence 
-                if shifts_left == periods_left:
-                    initial_cost += min_cost
-                if shifts_left == 0:
-                    initial_cost = sys.maxsize
+    schedule = defaultdict(list)
 
-                costs = self.calc_costs(request, initial_cost)
-                cost_matrix.append(costs)
+    for period in range(periods):
+        cost_matrix = []
+        periods_left = periods - period
 
-            # Use the hungarian algorithm to calculate the optimal worker
-            # for each job at this period
-            row_ind, col_ind = linear_sum_assignment(cost_matrix)
-            size = len(row_ind)
+        for ndx, request in enumerate(requests):
+            initial_cost = PRECEDENCE_WEIGHT * ndx
+            shifts_left = counts[request.Name]
 
-            workers = [self.requests[i].Name for i in row_ind]
-            jobs = [self.jobs[i] for i in col_ind]
+            # If a worker has as many shifts left as periods,
+            # they get highest precedence 
+            if shifts_left == periods_left:
+                initial_cost += min_cost
+            if shifts_left == 0:
+                initial_cost = sys.maxsize
 
-            for w,j in zip(workers, jobs):
-                pool[w] -= 1
-                self.schedule[j].append(w)
+            costs = calc_costs(request, initial_cost, jobs)
+            cost_matrix.append(costs)
 
-    def to_csv(self, dest):
-        with open(dest, 'w') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow([''] + list(range(1, self.periods + 1)))
-            for job in self.schedule:
-                writer.writerow([job] + self.schedule[job])
+        # Use the hungarian algorithm to calculate the optimal worker
+        # for each job at this period
+        row_ind, col_ind = linear_sum_assignment(cost_matrix)
+
+        workers = [requests[i].Name for i in row_ind]
+        jobs = [jobs[i] for i in col_ind]
+
+        for w,j in zip(workers, jobs):
+            counts[w] -= 1
+            schedule[j].append(w)
+
+    return schedule
+
+
+def to_csv(schedule, dest):
+    with open(dest, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for job in schedule:
+            writer.writerow([job] + schedule[job])
 
 
 if __name__ == '__main__':
@@ -143,6 +144,7 @@ if __name__ == '__main__':
         data = namedtuple('Request', next(reader))
         requests = [row for row in map(data._make, reader)]
 
-    s = Scheduler(args.periods, workers, jobs, requests)
-    s.assign_schedule()
-    s.to_csv(args.output)
+    requests = sort_requests(requests, workers)
+
+    schedule = assign_schedule(args.periods, requests, jobs)
+    to_csv(schedule, args.output)
