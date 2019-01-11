c6974a8d6dde7b5512a46f6889dce431038e7f8d
nwinston <noahwinston@Noahs-MacBook-Pro-2.local>
2019-01-10 21:17:18 -0500

    cleanup

--- scheduler.py
+++ scheduler.py
@@ -35,10 +35,10 @@ class Scheduler:
     def __init__(self, periods, workers, jobs, requests):
         '''
         Args:
-            periods: number of periods to schedule
-            workers: list of workers in order of precedence
-            jobs: list of jobs
-            requests: list of worker requests
+            - number of periods to schedule
+            - list of workers in order of precedence
+            - list of jobs
+            - list of worker requests
         '''
         self.periods = periods
         self.workers = workers
